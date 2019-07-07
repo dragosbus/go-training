040488f3e598031a1f883381425210f7685c8dc0
dragosbus <dragosbusuioc94@gmail.com>
2019-07-07 10:46:35 +0200

    fizzbuz using switch

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -13,3 +13,17 @@ func fizzbuzz(x int) interface{} {
 
 	return x
 }
+
+func fizzbuzSwitch(x int) interface{} {
+	//if the cases are coerced to a bool, the expresion can be avoided
+	switch {
+	case x%3 == 0 && x%5 == 0:
+		return "fizzbuz"
+	case x%3 == 0:
+		return "fizz"
+	case x%5 == 0:
+		return "buzz"
+	default:
+		return x
+	}
+}
